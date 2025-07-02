4bb3217c52579ba9d532be4012382d6080b719e9
jahnavi-dotcom <jahnavi.1254@gmail.com>
2025-07-02 15:55:31 +0530

    c

--- server/server.ts
+++ server/server.ts
@@ -34,9 +34,7 @@ connectCloudinary();
 const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://job-portal-client-jade-one.vercel.app",
-  "https://job-portal-client-jvcoi7c4w-bhargavis-projects-6383a334.vercel.app",
-  "https://job-portal-client-k11gtoezd-bhargavis-projects-6383a334.vercel.app" 
+  
 ];
 
 app.use(cors({
